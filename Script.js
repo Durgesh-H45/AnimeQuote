a12af09e85cb164f3a7107192bffb00dd4b92101
Durgesh <hirparadurgesh1@gmail.com>
2026-03-10 17:27:38 +0530

    Initial commit

---
+++ Script.js
@@ -0,0 +1,19 @@
+async function getQuote() {
+
+ // const url = "https://animechan.xyz/api/random";
+ const url = "https://api.animechan.io/v1/quotes/random";
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    document.getElementById("quote").innerText = `"${data.data.content}"`;
+    document.getElementById("character").innerText = data.data.character.name;
+    document.getElementById("anime").innerText = data.data.anime.name;
+
+  } catch (error) {
+    document.getElementById("quote").innerText = "...Only 5 quotes allowed...";
+  }
+}
+
+getQuote();
